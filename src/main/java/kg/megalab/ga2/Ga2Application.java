0365ad793c27b6286396fbe13d85435556f00489
kairo301004 <bishkekkairat@gmail.com>
2025-03-21 17:00:14 +0300

    first commit

---
+++ src/main/java/kg/megalab/ga2/Ga2Application.java
@@ -0,0 +1,13 @@
+package kg.megalab.ga2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ga2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ga2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kg/megalab/ga2/Ga2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.megalab.ga2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ga2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
